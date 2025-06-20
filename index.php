f17784f21a9c47ac9f019d55f8feac7fce355fb1
fahmizulhan <91739367+fahmizulhan@users.noreply.github.com>
2025-06-20 16:29:16 +0700

    commitdua

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Index</title>
+</head>
+<body>
+    <h1>Selamat datang di website saya!</h1>
+    <p>Ini adalah contoh halaman index.php sederhana.</p>
+
+    <?php
+    echo "<p>Tanggal hari ini: " . date("Y-m-d") . "</p>";
+    ?>
+
+</body>
+</html>
